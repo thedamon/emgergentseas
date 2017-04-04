68a58dc2515968de5d26cd4f245b9263527c3075
Damon Muma <damuma@gmail.com>
2017-04-04 00:45:28 -0400

    a bunch of kickass changes that will knock you on your ass

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i|Space+Mono:400,400i' }
     ]
   },
--- pages/index.vue
+++ pages/index.vue
@@ -6,51 +6,52 @@
       <header>
         <p><strong>EmergentSeas</strong> presents</p>
         <h1>My Planet and Me</h1>
-        <h2>In which a sad millennial receives telepathic communications from a distant planet.</h2>
+        <h2>in which a sad millennial receives telepathic communications from a distant planet</h2>
 
-        <p></p>
-
-        <img src="../assets/floor.jpg" alt="Damon lying on a wooden floor looking overwhelmed">
+        <p class="shouty">A speculative, darkly comedic solo show by Damon Muma premiering at the 2017 London Fringe Theatre Festival.</p>
       </header>
+
+      <img src="../assets/images/mosaic.jpg" alt="Some photographs of Damon looking like a fucking hipster lying on the floor">
 
       <!-- <section class="links">
         <a href="">Buy tickets</a>
         <a href="">Facebook</a>
       </section> -->
+      <main>
+        <section class="info">
+         <div class="info-block">
+            <ul class="credits">
+              <li>Produced, written and performed by <span>Damon Muma</span></li>
+              <li>Directed by <span>Kristin Bennett, Cara Moyer and Damon Muma</span></li>
+              <li>Original Sounds: <span><a href="https://wormwoodmusic.bandcamp.com/">Wormwood</a></span></li>
+              <li>Stage Manager: <span>Cara Moyer</span></li>
+              <li>Dramaturge: <span>Kristin Bennett</span></li>
+              <li>Movement Coach: <span><a href="https://hannahgoldberg.wixsite.com/dance">Hannah Goldberg</a></span></li>
+              <li>Poster and logo artwork: <span><a href="http://cargocollective.com/RobinHenry">Robin Henry</a></span></li>
+              <li>Trailer production: <span>Pam Haasen, Matt Dupont</span></li>
+            </ul>
+          </div>
+          <div class="info-block">
+            <p>All performances at <a href="http://theartsproject.ca">The Arts Project</a>, 203 Dundas St, London</p>
 
-      <section class="info">
-        <div class="info-block">
-          <p>All performances at The Arts Project, 203 Dundas St, London</p>
+            <ul class="showtimes">
+              <li>Thurs, June 1 7:00pm</li>
+              <li>Sat, June 3 5:00pm</li>
+              <li>Sun, June 4 9:00pm</li>
+              <li>Tues, June 6 6:30pm</li>
+              <li>Fri, June 9 8:00pm</li>
+              <li>Sat, June 10 1:00pm</li>
+            </ul>
 
-          <ul class="showtimes">
-            <li>Thurs, June 1 7:00pm</li>
-            <li>Sat, June 3 5:00pm</li>
-            <li>Sun, June 4 9:00pm</li>
-            <li>Tues, June 6 6:30pm</li>
-            <li>Fri, June 9 8:00pm</li>
-            <li>Sat, June 10 1:00pm</li>
-          </ul>
+            <p>Tickets available May 1st at <a href="http://londonfringe.ca">the Fringe website</a></p>
+          </div>
 
-          <p>Tickets available May 1st at <a href="http://londonfringe.ca">the Fringe website</a></p>
-        </div>
-        <div class="info-block">
-          <ul class="credits-primary">
-            <li>Produced, written and performed by <span>Damon Muma</span></li>
-            <li>Directed by <span>Kristin Bennett, Cara Moyer and Damon Muma</span></li>
-            <li>Original Sounds: <span><a href="https://wormwoodmusic.bandcamp.com/">Wormwood</a></span></li>
-            <li>Stage Manager: <span>Cara Moyer</span></li>
-            <li>Dramaturge: <span>Kristin Bennett</span></li>
-            <li>Movement Coach: <span><a href="https://hannahgoldberg.wixsite.com/dance">Hannah Goldberg</a></span></li>
-            <li>Poster and logo artwork: <span><a href="http://cargocollective.com/RobinHenry">Robin Henry</a></span></li>
-            <li>Trailer: <span>Pam Haasen, Matt Dupont</span></li>
-          </ul>
-
-          <h2>Very Special Thanks:</h2>
-          <p>
-            Emma Bulpin, Laurie Bursch, Valerie Cotic, Jason Dickson, Caleb Heddle, Pam Haasen, Antony Hare, Dorit Osher, Jon Patterson, Kara Riepart, Savannah Sewell, Margot Stothers, Matthew Trueman
-          </p>
-        </div>
-      </section>
+        </section>
+<!--         <h2>Very Special Thanks:</h2>
+        <p>
+          Emma Bulpin, Laurie Bursch,  Valerie Cotic, Jason Dickson,  Caleb Heddle, Pam Haasen, Antony Hare, Dorit Osher, Jon Patterson, Kara Riepart, Savannah Sewell,  Margot Stothers, Matthew Trueman - Nicholas Veit
+        </p> -->
+      </main>
     </div>
 </div>
 </template>
@@ -60,9 +61,12 @@
     background-color: black;
     color: white;
     font-family: 'Open Sans', sans-serif;
-    font-weight: 400;
+    font-weight: 300;
     line-height: 1.3;
     font-size: 20px;
+  }
+  body {
+    margin: 0;
   }
   h1, h2, h3, h4 {
     font-weight: normal;
@@ -115,11 +119,18 @@
   }
 
   .container {
-    padding: 3vw 6vw;
-    margin: 0vw 12vw ;
+    margin: 0vw 10vw ;
     color: white;
     /*background-color: rgba(255,255,255,.2);*/
     background: linear-gradient(328deg, rgba(0,0,0,0.15) 0%, rgba(255,255,255,0.15) 100%);
+  }
+
+  header {
+    padding: 3vw 4vw;
+  }
+
+  main {
+    padding: 2vw 4vw;
   }
 
   .info {
@@ -131,10 +142,35 @@
     width: 100%;
   }
 
+  p:first-child {
+    margin-top: 0;
+  }
+
+  @media (min-width: 960px){
+    .info {
+      display: flex;
+    }
+  }
+
+  .credits li {
+    text-transform: uppercase;
+    font-size: 14px;
+    margin-bottom: 20px;
+  }
+
+  .credits li span {
+    text-transform: none;
+    font-size: 28px;
+    line-height: 1.1;
+    display: block;
+  }
+
+
   ul, ol {
     list-style: none;
     margin: 0;
     padding: 0;
+    line-height: 1.6;
   }
 
   @keyframes flicker {
