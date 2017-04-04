aa6fcc2e5785d84b96c5cc0d07aed10fca59303b
Damon Muma <damuma@gmail.com>
2017-04-03 22:28:43 -0400

    Convert existing code to Nuxt, no foolin

---
+++ nuxt.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'EmergentSeas presents My Planet and Me',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i|Space+Mono:400,400i' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLINT on save
+    */
+    extend (config, ctx) {
+      if (ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
