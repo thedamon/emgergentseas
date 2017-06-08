2b90b0e8dbeee7b53e21a884de639313dde61a23
Damon Muma <damuma@gmail.com>
2017-06-08 16:05:14 -0400

    fix it

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,9 @@ module.exports = {
       { href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i|Space+Mono:400,400i' }
     ]
   },
+  plugins: [
+    { src: '~plugins/ga.js', ssr: false }
+  ],
   /*
   ** Customize the progress-bar color
   */
@@ -25,15 +28,15 @@ module.exports = {
     /*
     ** Run ESLINT on save
     */
-    extend (config, ctx) {
-      if (ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
+    // extend (config, ctx) {
+    //   if (ctx.isClient) {
+    //     config.module.rules.push({
+    //       enforce: 'pre',
+    //       test: /\.(js|vue)$/,
+    //       loader: 'eslint-loader',
+    //       exclude: /(node_modules)/
+    //     })
+    //   }
+    // }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,78 @@
 
 <template>
+<!-- submit your PRs at: https://github.com/thedamon/emergentseas -->
   <div class="index">
     <pre aria-hidden="true">✷  · 　 　      　　.  　　 　　 　    　✵   ✷  　　  ·     　　  　　 　　✵  *  .         　　　　.  　 　 　　     　* 　　　 　　 · ✦         ✦ .· 　　 ·      ✦  ✵  · . 　  　   *      .          * 　 　　　　✷     .    　.         ·  　 .   　·. ✷   ⋆   ✹  . 　　 ✵      +  ˚      * 　　　　　* 　　　　　　  ⋆ 　　 ✧ *  · 　 . 　 ✧ ˚ 　 *   　 +  ˚   · .  .　　*  * 　 ˚  .　　　　　 +  　　　 *  *　　 　 　.  ·✵  . 　   ˚ . 　  ✵　 　　　 　　　　.  ˚ 　  　  　 ✧ ·  　　 ⊹ *  .  　　  　 　  　 · 　　　　　　　 ✦  ⊹ 　  　 　  　　   ✵                         ˚ 　　        ✹   .  　　   ✦ 　 *     　　  ✹  ˚   　   ·.　    　　 *　✷   　　　　　    　　  　　 .  　* ·  　     ✺ 　　　　　　　　.   *  　    　 ˚  　　 ✧    ⋆  *   　　 ✦  ·     　 *   . 　✵   *　     ··  　　 　　 　　 　　　　    　　　　 · . .  .  ✦ 　      ✵ * .  ·　　　　　 . 　　　     · ˚ 　 ·     ✵   　　 .　　 　　 . ·     .  .   　     ˚     　 　 ˚  *  ˚     　　. · * 　 　　　　    　 　 　 ⋆ 　　　　 　　     　   　　　 . 　　　  . 　　    　　  ⋆ 　　　　　 .    ˚ 　 　　　　　　　✷      　　 · 　　　　　     　　  　  　 　 　  　 ⋆     · 　*  .  ✧ ˚ 　 .  ✦ *      * 　· 　  　　 　　　    　 ·  ·  　 　✷        　 ✫ · *  ✹    </pre>
     <div class="container">
       <header>
-        <p><strong>EmergentSeas</strong> presents</p>
-        <h1>My&nbsp;Planet and&nbsp;Me</h1>
-        <h2>in which a sad millennial receives telepathic communications from a distant planet</h2>
+        <p class="presenter"><strong>EmergentSeas</strong> presents</p>
+        <h1>My&nbsp;Planet &amp;&nbsp;Me</h1>
 
-        <p class="shouty">A speculative, darkly comedic solo show by Damon Muma premiering at the 2017 London Fringe Theatre Festival.</p>
+        <p class="shouty">premiering at the 2017 London Fringe Theatre Festival</p>
       </header>
 
-      <img src="../assets/images/mosaic.jpg" alt="Some photographs of Damon looking like a fucking hipster lying on the floor">
+      <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/lYFZSF-NJiM' frameborder='0' allowfullscreen></iframe></div>
 
-      <!-- <section class="links">
-        <a href="">Buy tickets</a>
-        <a href="">Facebook</a>
-      </section> -->
       <main>
+        <section class="ctas">
+          <a href="http://londonfringe.ca/festivals/london-fringe-festival/events/my-planet-and-me">Fringe</a>
+          <a href="https://www.facebook.com/events/212879372535108/">RSVP</a>
+          <a href="https://tickets.londonfringe.ca/TheatreManager/1/login&event=284">Tickets</a>
+        </section>
         <section class="info">
-         <div class="info-block">
+          <div class="info-block">
+            <hr>
+            <p class="praise">“&nbsp;A wildly entertaining performer — <a href="https://theatreinlondon.ca/2017/06/my-planet-and-me/#comment-1672">★&nbsp;★&nbsp;★&nbsp;★</a>&nbsp;”</p>
+            <p class="praise">“&nbsp;A standout show — <a href="https://theatreinlondon.ca/2017/06/my-planet-and-me/#comment-1686">★&nbsp;★&nbsp;★&nbsp;★</a>&nbsp;”</p>
+            <p class="praise">“&nbsp;<a href="https://theatreinlondon.ca/2017/06/my-planet-and-me/#comment-1603">★&nbsp;★&nbsp;★&nbsp;★</a>&nbsp;”</p>
+            <p class="praise">“&nbsp;<a href="https://theatreinlondon.ca/2017/06/my-planet-and-me/#comment-1676">★&nbsp;★&nbsp;★&nbsp;★</a>&nbsp;”</p>
+            <p class="praise">“&nbsp;Too effective <a href="http://www.lfpress.com/2017/06/08/london-fringe-my-planet-and-me-written-and-performed-by-damon-muma-directed-by-kristin-bennett-and-cara-moyer">🤓</a>&nbsp;”</p>
+            <hr>
+            <table class="showtimes">
+              <tr><td>Thu,</td><td>June 1</td><td>7:00pm</td></tr>
+              <tr><td>Sat,</td><td>June 3</td><td>5:00pm</td></tr>
+              <tr><td>Sun,</td><td>June 4</td><td>9:00pm</td></tr>
+              <tr><td>Tue,</td><td>June 6</td><td>6:30pm</td></tr>
+              <tr><td>Fri,</td><td>June 9</td><td>8:00pm</td></tr>
+              <tr><td>Sat,</td><td>June 10</td><td>1:00pm</td></tr>
+            </table>
+
+            <p>at <a href="http://theartsproject.ca">The Arts Project</a>,<br>203 Dundas St, London</p>
+            <p>Tickets available at the <a href="http://londonfringe.ca/festivals/london-fringe-festival/events/my-planet-and-me">Fringe&nbsp;website</a></p>
+            <p>Twitter: <a href="http://twitter.com/_emergent_seas">@_emergent_seas</a></p>
+            <p>Instagram: <a href="https://www.instagram.com/explore/tags/myplanetandme/">#myplanetandme</a></p>
+          </div>
+          <div class="info-block">
             <ul class="credits">
               <li>Written and performed by <span>Damon Muma</span></li>
               <li>Directed by <span>Kristin Bennett, <br>Cara Moyer and <br>Damon Muma</span></li>
-              <li>Original Sounds: <span><a href="https://wormwoodmusic.bandcamp.com/">Wormwood</a></span></li>
+              <li>Original Soundtrack: <span><a href="https://wormwoodmusic.bandcamp.com/">Wormwood</a></span></li>
               <li>Stage Manager: <span>Cara Moyer</span></li>
               <li>Dramaturge: <span>Kristin Bennett</span></li>
-              <li>Movement Coach: <span><a href="https://hannahgoldberg.wixsite.com/dance">Hannah Goldberg</a></span></li>
-              <li>Poster and logo artwork: <span><a href="http://cargocollective.com/RobinHenry">Robin Henry</a></span></li>
+              <li>Movement Director: <span><a href="https://hannahgoldberg.wixsite.com/dance">Hannah Goldberg</a></span></li>
+              <li>Set Design: <span><a href="http://www.matthewtrueman.com/">Matthew Trueman</a></span></li>
+              <li>Additional voices: <span>Kristin Bennett, Christina Willatt, Kara Riepart</span></li>
               <li>Trailer production: <span>Pam Haasen, Matt Dupont</span></li>
+              <li>Poster artwork: <span><a href="http://cargocollective.com/RobinHenry">Robin Henry</a></span></li>
             </ul>
           </div>
-          <div class="info-block">
 
-            <ul class="showtimes">
-              <li>Thurs, June 1 7:00pm</li>
-              <li>Sat, June 3 5:00pm</li>
-              <li>Sun, June 4 9:00pm</li>
-              <li>Tues, June 6 6:30pm</li>
-              <li>Fri, June 9 8:00pm</li>
-              <li>Sat, June 10 1:00pm</li>
-            </ul>
-
-            <p>at <a href="http://theartsproject.ca">The Arts Project</a>,<br>203 Dundas St, London</p>
-            <p>Tickets available May 1st at the <a href="http://londonfringe.ca">Fringe&nbsp;website</a></p>
-            <p>15 minute excerpt at <a href="https://www.facebook.com/events/761987920642197/">Open Open Open</a> in Toronto on April 27</p>
-            <p>Trailer premiere: Kangaroo Variety, April 29</p>
-            <!-- <p>A zine illustrated by Antony Hare: beginning of May</p> -->
-            <br>
-          </div>
 
         </section>
-          <p>Follow developments on Twitter: <a href="http://twitter.com/_emergent_seas">@_emergent_seas</a></p>
-<!--         <h2>Very Special Thanks:</h2>
+
+
+
+        <h2 class="new-section">Very Special Thanks:</h2>
         <p>
-          Emma Bulpin, Laurie Bursch,  Valerie Cotic, Jason Dickson,  Caleb Heddle, Pam Haasen, Antony Hare, Dorit Osher, Jon Patterson, Kara Riepart, Savannah Sewell,  Margot Stothers, Matthew Trueman - Nicholas Veit
-        </p> -->
+          Emma Bulpin, Valerie Cotic, Jason Dickson, Antony Hare, Kathy Navackas, Dorit Osher, Jon Paterson, Dave Rino, Margot Stothers, Moxies Grill and Bar, Nicholas Veit
+        </p>
       </main>
+      <img src="../assets/images/mosaic.jpg" alt="Some photographs of Damon looking like a fucking hipster lying on the floor">
     </div>
 </div>
 </template>
 
-<style>
+<style lang="scss">
   /*@mixin f($px){
     font-size: calc($px - 10 + 5vw);
   }*/
@@ -81,18 +92,32 @@
   }
   h1 {
     margin: 0;
-    font-size: calc(27px + 4.7vw);
+    font-size: calc(17px + 6.9vw);
     font-family: 'Space Mono', monospace;
     font-style: italic;
     line-height: 1;
-    text-align: center;
     word-spacing: calc(-17px + 1vw);
+
+    @media (min-width: 2048px){
+      font-size: 147px;
+    }
+  }
+
+  .presenter {
+    font-size: calc(13px + 1vw);
+  }
+
+  header p {
+    margin: 0;
+  }
+  header h1 + * {
+    margin-top: 16px;
+  }
+  @media (min-width: 2192px){
+    h1 {font-size: 130px;}
   }
   h2 {
     font-size: calc(18px + 1vw);
-  }
-  header p {
-    margin: 0;
   }
   header h2 {
     margin: 10px 0;
@@ -111,7 +136,18 @@
     background: url('../assets/starfield.jpg');
     background-size: cover;
   }
-  a {color: white;}
+
+  a {
+    color: white;
+    text-decoration: none;
+    transition: all .5s;
+    border-bottom: 1px solid rgba(255,255,255,0.45);
+
+    &:hover {
+      border-bottom: 1px solid rgba(255,255,255,0.65);
+    }
+  }
+
   pre {
     /*animation: flicker .5s infinite;*/
     color: rgba(255,255,255,0.2);
@@ -135,8 +171,8 @@
 
   .container {
     margin: 0 auto;
-    width: 80vw;
-    min-width: 310px;
+    width: calc(75vw + 80px);
+    min-width: 300px;
     max-width: 1440px;
     color: white;
     /*background-color: rgba(255,255,255,.2);*/
@@ -152,8 +188,8 @@
   }
 
   .info {
-    text-align: center;
-    font-size: calc(13px + 1.5vw);
+    /*text-align: center;*/
+    font-size: calc(13px + 1.2vw);
   }
 
   .info p {
@@ -168,12 +204,44 @@
     margin-top: 0;
   }
 
+  .ctas {
+    display: flex;
+    justify-content: space-between;
+    margin: 10px 0 40px;
+  }
+
+  .ctas a {
+    background-color: rgba(0,0,0,.4);
+    border: solid 4px rgba(255,255,255,.2);
+    padding: calc(4px + 1.8vw) 5vw;
+    text-decoration: none;
+
+    @media (min-width: 1814px){
+      padding: 36px 90px;
+    }
+  }
+
+  .ctas a:hover {
+    background-color: rgba(0,0,0,.8);
+    border-color: rgba(255,255,255,.4);
+  }
+
+  .showtimes {
+    td {
+      border: none;
+    }
+  }
+
   @media (min-width: 960px){
     .info {
       display: flex;
     }
     .info > div {
       width: 50%;
+
+      &:first-child {
+        padding-right: 5vw;
+      }
     }
   }
 
@@ -181,30 +249,53 @@
     margin-bottom: 48px;
   }
 
+  .praise {
+     font-size: calc(18px + 1vw);
+     a {
+      text-decoration: none;
+      border-bottom: none;
+      text-shadow: #eee 0px 0px 26px;
+      transition: all .4s ease-in;
+      transform: scale(1) rotate(0deg);
+
+      &:hover, &:active {
+        border-bottom: none;
+        transition: all .75s ease-in;
+        transform: scale(1.4) rotate(360deg);
+        text-shadow: #fff 0 0 26px;
+        display: inline-block;
+      }
+    }
+  }
+
   .credits li {
     text-transform: uppercase;
-    font-size: calc(13px + .75vw);
-    margin-bottom: calc(18px + .5vw);
+    font-size: calc(13px + .65vw);
+    margin-bottom: calc(15px + .3vw);
   }
 
   .credits li:first-child {
-    font-size: calc(15px + .75vw);
+    font-size: calc(15px + .55vw);
   }
 
   .credits li:first-child span {
-    font-size: calc(30px + 1.5vw);
+    font-size: calc(30px + 1.2vw);
   }
 
   .credits li span {
     text-transform: none;
-    font-size: calc(21px + 1vw);
+    font-size: calc(21px + .8vw);
     line-height: 1.1;
     display: block;
   }
 
   main p {
-    text-align: center;
-    font-size: calc(18px + .9vw);
+    /*text-align: center;*/
+    font-size: calc(18px + .7vw);
+  }
+
+  .new-section {
+    margin-top: calc(10px + 2vh);
   }
 
   ul, ol {
